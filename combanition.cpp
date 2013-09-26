8773139972f6ea825bd2284e74b57a96d1ae2e4a
DesByte <loki_84@163.com>
2013-09-26 17:48:51 +0800

    关于组合数程序的更新

--- combanition.cpp
+++ combanition.cpp
@@ -1,32 +1,21 @@
 #include "stdafx.h"
 #include<iostream>
 using namespace std;
+int Jie(int a)
+{
+	if(a==1)
+		return 1;
+	else return a*Jie(a-1);
+}
 int main()
 {   
-	int m=1,n=1,i=1;
-	int A_m=1,A_n=1,A_mn=1,C_mn=1;
-	
-    cout<<"请输入m的值：";
-	cin>>A_m;
-	cout<<"请输入n的值：";
-	cin>>A_n;
-	
-	C_mn=A_m/(A_n*A_mn);
-	
-	for(i=m;i>=1;i--)//计算m的全排列
-	{
-		A_m*=A_m;
-	}
-	
-	for(i=n;i>=1;i--)//计算n的全排列
-	{
-		A_n*=A_n;
-	}
-
-	for(i=m-n;i>=1;i--)//计算m-n的全排列
-	{
-		A_mn*=A_mn;
-	}
-	cout<<"从M个元素中取N个元素的组合个数是："<<C_mn;
+	int m,n;
+	int c;//C即为组合数
+	cout<<"请输入m的值:";
+	cin>>m;
+	cout<<endl<<"请输入n的值:";
+	cin>>n;
+	c=Jie(m)/(Jie(n)*Jie(m-n));
+	cout<<endl<<"组合数为:"<<c<<endl;
 }
 
